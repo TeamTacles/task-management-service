1e375f9b77054a720b2d50f444ffe087cf0c0156
Gabriela Santana <gabisantanadev@gmail.com>
2025-06-27 20:34:55 -0300

    ✨ starting

---
+++ src/main/java/com/teamtacles/task/teamtacles_api_task/TeamtaclesApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.teamtacles.task.teamtacles_api_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtaclesApiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamtaclesApiTaskApplication.class, args);
+	}
+
+}
