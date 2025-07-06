8dff99a05b7f08c567bd8606ae6b84f59df96a84
Gabriela Santana <gabisantanadev@gmail.com>
2025-07-06 20:44:15 -0300

    ✨ swagger update

--- src/main/java/com/teamtacles/task/teamtacles_api_task/TeamtaclesApiTaskApplication.java
+++ src/main/java/com/teamtacles/task/teamtacles_api_task/TeamtaclesApiTaskApplication.java
@@ -3,6 +3,10 @@ package com.teamtacles.task.teamtacles_api_task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "TeamTacles API Task", version = "1.0", description = "TeamTacles Task API Documentation – Task Management"))
 @SpringBootApplication
 public class TeamtaclesApiTaskApplication {
 
